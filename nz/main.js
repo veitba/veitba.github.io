7720b92e15d44d23007584e936906187e31c2064
Badde <csaw2513@uibk.ac.at>
2019-03-20 18:14:15 +0100

    auf Ausschnitt zoomen

--- nz/main.js
+++ nz/main.js
@@ -11,3 +11,9 @@ const titel = div.getAttribute("data-title");
 // Karte initialisieren
 let karte = L.map("map");
 // console.log(karte);
+
+// auf Ausschnitt zoomen
+karte.setView(
+    [breite,laenge]
+    13
+);
