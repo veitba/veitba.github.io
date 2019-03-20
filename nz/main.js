a7c983c211441bb7e9e9b989a23a812d1acd09ca
Badde <csaw2513@uibk.ac.at>
2019-03-20 18:04:48 +0100

    leaflet einbinden und Daten auslesen

---
+++ nz/main.js
@@ -0,0 +1,9 @@
+// alert("Hallo Welt")
+
+const div = document.getElementById("map")
+const lat = div.getAttribute("data-lat")
+const lng = div.getAttribute("data-lng")
+const title = div.getAttribute("data-title")
+
+
+// console.log("Breite =",lat,"Länge =",lng,"Titel =",title)
