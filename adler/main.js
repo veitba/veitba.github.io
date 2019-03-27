33a087b97a5fe4209b80ca4db3ac4977dd2238ac
Badde <csaw2513@uibk.ac.at>
2019-03-27 15:44:54 +0100

    Start Adler

---
+++ adler/main.js
@@ -0,0 +1,30 @@
+// alert("Hallo Welt")
+
+const div = document.getElementById("map");
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
+
+
+// console.log("Breite =",breite,"Länge =",laenge,"Titel =",titel)
+
+// Karte initialisieren
+let karte = L.map("map");
+// console.log(karte);
+
+// auf Ausschnitt zoomen
+karte.setView(
+    [breite,laenge],
+    13
+);
+
+// Open Streetmap einbauen
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+// Positionsmarker hinzufügen
+let pin=L.marker(
+   [breite,laenge]
+).addTo(karte);
+
+// Popup zum Pin hängen
+pin.bindPopup(titel).openPopup();
