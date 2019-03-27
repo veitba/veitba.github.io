18c4e5c5df577bc0ee2af7c9aa4cbb618bfa8d28
Badde <csaw2513@uibk.ac.at>
2019-03-27 16:34:57 +0100

    Karte initialisieren

--- adler/main.js
+++ adler/main.js
@@ -5,6 +5,13 @@ const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const titel = div.getAttribute("data-title");
 
+// 2. Punkt
+
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
+
+
 
 // console.log("Breite =",breite,"Länge =",laenge,"Titel =",titel)
 
@@ -14,17 +21,22 @@ let karte = L.map("map");
 
 // auf Ausschnitt zoomen
 karte.setView(
-    [breite,laenge],
-    13
+    [breite, laenge],
+    12
 );
 
 // Open Streetmap einbauen
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
 
 // Positionsmarker hinzufügen
-let pin=L.marker(
-   [breite,laenge]
+let pin = L.marker(
+    [breite, laenge]
+).addTo(karte);
+
+let pin2 = L.marker(
+    [breite2, laenge2]
 ).addTo(karte);
 
 // Popup zum Pin hängen
 pin.bindPopup(titel).openPopup();
+pin2.bindPopup(titel2).openPopup();
