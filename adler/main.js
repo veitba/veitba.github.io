380f04286bfe22215e6b9aaab25caef7847ad40e
Badde <csaw2513@uibk.ac.at>
2019-04-03 18:06:46 +0200

    Vollbild & Koordinaten

--- adler/main.js
+++ adler/main.js
@@ -24,11 +24,73 @@ let karte = L.map("map");
 //     [47.2, 11.2],
 //     8
 // );
+//Karten einbauen
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    bmapoberflaeche: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoberflaeche/grau/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://www.basemap.at"> basemap.at</a>'
+    }),
+    stamen_toner : L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains : ["a","b","c"],
+        attribution : 'Datenquelle: Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>'
+    }),
+    stamen_terrain : L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains : ["a","b","c"],
+        attribution : 'Datenquelle: Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>'
+    }),
+    stamen_watercolor : L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains : ["a","b","c"],
+        attribution : 'Datenquelle:  Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.</a>'
+    })
+};
 
 // Open Streetmap einbauen
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png",{
-    subdomains:["a","b","c"],
-    attribution:'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+
+
+
+kartenLayer.stamen_terrain.addTo(karte);
+
+//Auswahlmenü hinzufügen
+L.control.layers({
+    "OpenStreetMap": kartenLayer.osm,
+    "Geoland Basecamp": kartenLayer.geolandbasemap,
+    "Geoland Basemap Overlay" : kartenLayer.bmapgrau,
+    "Geoland Basemap Grau" : kartenLayer.bmapoverlay,
+    "Geoland Basemap hidpi" : kartenLayer.bmaphidpi,
+    "Geoland Basemap Foto" : kartenLayer.bmaporthofoto30cm,
+    "Geoland Basemap Gelände" : kartenLayer.bmapgelaende,
+    "Geoland Basemap Fläche" : kartenLayer.bmapoberflaeche,
+    "Stamen Maps Toner" : kartenLayer.stamen_toner,
+    "Stamen Maps Terrain" : kartenLayer.stamen_terrain,
+    "Stamen Maps Watercolor" : kartenLayer.stamen_watercolor
 }).addTo(karte);
 
 // Fernrohre
@@ -63,6 +125,8 @@ for (let blick of ADLERBLICKE) {
             <em>Kunde: ${blick.kunde}</em>`
     );
 }
-console.log(blickeGruppe.getBounds());
+//console.log(blickeGruppe.getBounds());
 // Auf Adlerblicke zoomen
 karte.fitBounds(blickeGruppe.getBounds());
+karte.addControl(new L.Control.Fullscreen());
+var hash = new L.Hash(karte);
