9edc8a0f25d6dd24bdc77e3130f8e0355eb338ef
veitba <youremail@address.comveitb>
2019-04-05 19:57:32 +0200

    NZ Karten

--- nz/main.js
+++ nz/main.js
@@ -29,6 +29,14 @@ const kartenLayer = {
     stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
         subdomains: ["a", "b", "c"],
         attribution: 'Datenquelle:  Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.</a>'
+    }),
+    nz_topo50: L.tileLayer("http://tiles-a.data-cdn.linz.govt.nz/services;key=YOUR_API_TOKEN/tiles/v4/layer=50767/EPSG:3857/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Datenquelle:  Map tiles by <a href="https://data.linz.govt.nz/services/csw/?service=CSW&amp;request=GetCapabilities">, under CC BY SA.</a>'
+    }),
+    nz_Aerial: L.tileLayer("http://tiles-a.data-cdn.linz.govt.nz/services;key=61f4f788d982486eb3bbc564cf1a8808/tiles/v4/set=4702/EPSG:3857/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Datenquelle:  Map tiles by <a href="https://data.linz.govt.nz/services/csw/?service=CSW&amp;request=GetCapabilities">, under CC BY SA.</a>'
     })
 };
 
@@ -43,7 +51,9 @@ L.control.layers({
     "OpenStreetMap": kartenLayer.osm,
     "Stamen Maps Toner": kartenLayer.stamen_toner,
     "Stamen Maps Terrain": kartenLayer.stamen_terrain,
-    "Stamen Maps Watercolor": kartenLayer.stamen_watercolor
+    "Stamen Maps Watercolor": kartenLayer.stamen_watercolor,
+    "NZ Topo 50": kartenLayer.nz_topo50,
+    "NZ Aerial Imagery": kartenLayer.nz_Aerial
 }).addTo(karte);
 
 
