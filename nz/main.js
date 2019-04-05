824c525ec90df54978e0f8d156fe0ba52de0bec9
veitba <youremail@address.comveitb>
2019-04-05 19:37:07 +0200

    NZ Karten einbauen

--- nz/main.js
+++ nz/main.js
@@ -12,6 +12,41 @@ const titel = div.getAttribute("data-title");
 let karte = L.map("map");
 // console.log(karte);
 
+//Karten einbauen
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    }),
+    stamen_toner: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Datenquelle: Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>'
+    }),
+    stamen_terrain: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Datenquelle: Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>'
+    }),
+    stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Datenquelle:  Map tiles by <a href="http://stamen.com">, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.</a>'
+    })
+};
+
+// Open Streetmap einbauen
+
+
+
+kartenLayer.stamen_terrain.addTo(karte);
+
+//Auswahlmenü hinzufügen
+L.control.layers({
+    "OpenStreetMap": kartenLayer.osm,
+    "Stamen Maps Toner": kartenLayer.stamen_toner,
+    "Stamen Maps Terrain": kartenLayer.stamen_terrain,
+    "Stamen Maps Watercolor": kartenLayer.stamen_watercolor
+}).addTo(karte);
+
+
 // auf Ausschnitt zoomen
 karte.setView(
     [breite,laenge],
@@ -26,5 +61,15 @@ let pin=L.marker(
    [breite,laenge]
 ).addTo(karte);
 
+
 // Popup zum Pin hängen
 pin.bindPopup(titel).openPopup();
+
+karte.addControl(new L.Control.Fullscreen()); // Fullscreen Plugin
+var hash = new L.Hash(karte); //Koordinaten anzeigen im Link
+
+var coords = new L.Control.Coordinates(); //Koordinaten durch Klick anzeigen
+coords.addTo(karte);
+karte.on('click', function (e) {
+    coords.setCoordinates(e);
+});
