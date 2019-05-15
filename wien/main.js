131e69fcd7528d9720934c12e2702901ae25be1b
Badde <csaw2513@uibk.ac.at>
2019-05-15 18:37:50 +0200

    Spazierwege

--- wien/main.js
+++ wien/main.js
@@ -73,26 +73,80 @@ karte.setView([48.208333, 16.373056], 12);
 
 const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD &srsName=EPSG:4326&outputFormat=json';
 
-function makeIcon(feature, latlng) {
-    return L.marker(latlng, {
-            icon: L.icon({
-                iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.svg',
-                iconSize: [36, 36]
-
-            })
-        })
-        .bindPopup(`
-<h3>${feature.properties.NAME}</h3>
-<p>${feature.properties.BEMERKUNG}</p>`);
+function makeMarker(feature, latlng) {
+    const fotoIcon = L.icon({
+        iconUrl: 'http://www.data.wien.gv.at/icons/sehenswuerdigogd.svg', //anderer Marker
+        iconSize: [16, 16]
+    });
+    const sightMarker = L.marker(latlng, {
+        icon: fotoIcon
+    });
+    sightMarker.bindPopup(`
+        <h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.BEMERKUNG}</p>
+        <hr>
+        <footer><a target="blank" href="${feature.properties.WEITERE_INF}">Weblink</a></footer>
+        `); //Name, Beschreibung, Weblink (neuer Tab)
+    return sightMarker;
 }
 
 async function loadSights(url) {
+    const clusterGruppe = L.markerClusterGroup();
     const response = await fetch(url);
     const sightsData = await response.json();
     const geoJson = L.geoJson(sightsData, {
-        pointToLayer: makeIcon
+        pointToLayer: makeMarker
     });
-    karte.addLayer(geoJson);
+
+    //Clustergruppe
+    clusterGruppe.addLayer(geoJson);
+    karte.addLayer(clusterGruppe);
+    layerControl.addOverlay(clusterGruppe, "Sehenswürdigkeit");
+
+    //Suchfeld einfügen
+    const suchFeld = new L.Control.Search({
+        layer: clusterGruppe,
+        propertyName: "NAME",
+        zoom: 17,
+        initial: false
+    });
+    karte.addControl(suchFeld);
 }
 
 loadSights(url);
+
+//Maßstab einfügen
+const scale = L.control.scale({
+    imperial: false,
+    metric: true
+
+});
+karte.addControl(scale);
+
+
+//Spazierwege hinzufügen
+const wege = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERLINIEOGD &srsName=EPSG:4326&outputFormat=json';
+
+function linienPopup(feature, layer) { //Wege Popup (nur Name möglich)
+    const popup = `
+        <h3>${feature.properties.NAME}</h3>
+        `;
+    layer.bindPopup(popup);
+}
+
+async function loadWege(wegeURL) {
+    const antwort = await fetch(wegeURL);
+    const wegeData = await antwort.json();
+    const wegeJson = L.geoJson(wegeData, {
+        style: function () { //Farbe der Wege
+            return {
+                color: "yellow"
+            };
+        },
+        onEachFeature: linienPopup
+    });
+
+    karte.addLayer(wegeJson);
+    layerControl.addOverlay(wegeJson, "Spazierwege");
+}
+loadWege(wege);
